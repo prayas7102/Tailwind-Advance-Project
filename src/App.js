133cb1187903d84ab1a86db2af205a06262879b3
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-10-27 08:39:26 +0530

    tailwind setup done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="text-2xl text-sky-500 font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
