a2e27ba53836f117fc14b0222e968949a1a8fac8
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-10-27 10:29:48 +0530

    navbar initialized

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl text-sky-500 font-bold underline">
-        Hello world!
-      </h1>
+      
     </div>
   );
 }
