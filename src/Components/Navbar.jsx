ebf685ce54b1a13b353f50cd6f7c4b17fa47171b
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-10-29 00:55:03 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex justify-between items-center bg-blue-500 p-4">
+
+      <div className="flex justify-between items-center md:order-2">
+        <div className="inline-block cursor-pointer p-4">
+          <div className="bg-black line h-1 w-6 my-1"></div>
+          <div className="bg-black line h-1 w-6 my-1"></div>
+          <div className="bg-black line h-1 w-6 my-1"></div>
+        </div>
+        <div className="md:hidden">Search</div>
+      </div>
+
+      <div className="text-center md:order-1 flex">Microsoft
+        <div className="md:flex mx-4 space-x-2 positio">
+          <div>Microsoft</div>
+          <div>365</div>
+          <div>Office</div>
+          <div>Windows</div>
+          <div>Xbox</div>
+        </div>
+      </div>
+      <div className="text-center md:order-3 flex justify-between">
+        <div className="hidden md:block">Search</div>
+        <div className="">Account</div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
